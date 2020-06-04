4516e5cc64475c86dc320bd3effcd40488ea6498
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-06-03 22:20:45 -0600

    added seeds and schema files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Plain Burger');
+
