45e8ab4cfcc98830a0176e06e9050fcca758b48d
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-06-07 00:32:53 -0600

    wprking on handlebars

--- models/burger.js
+++ models/burger.js
@@ -1,17 +1,17 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function(cb) {
+    selectAll: function(cb) {
       orm.selectAll("burgers", function(res) {
         cb(res);
       });
     },
-    create: function(cols, vals, cb) {
+    insertOne: function(cols, vals, cb) {
       orm.insertOne("burgers", cols, vals, function(res) {
         cb(res);
       });
     },
-    update: function(objColVals, condition, cb) {
+    updateOne: function(objColVals, condition, cb) {
       orm.updateOne("cats", objColVals, condition, function(res) {
         cb(res);
       });
